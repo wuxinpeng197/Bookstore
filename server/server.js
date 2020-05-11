6f797d6865c9079a002b4e4acfcff5c770893452
unknown <xinpengwu193@gmail.com>
2020-05-11 14:20:04 +1000

    config server

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'))
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
@@ -545,6 +547,14 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
         }
     )
 })
+
+// DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3002
 
